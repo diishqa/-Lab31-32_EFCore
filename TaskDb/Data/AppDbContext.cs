0634c2bd0ba7d12234ae5a407f95f5e49507d690
diishqa <diana.34@list.ru>
2026-04-24 20:17:17 +0300

    Step 3: AppDbContext and TaskItem model created

---
+++ TaskDb/Data/AppDbContext.cs
@@ -0,0 +1,48 @@
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using TaskDb.Data;
+using TaskDb.Models;
+namespace TaskDb.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<TaskItem> Tasks {get; set;}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<TaskItem>().HasData(
+            new TaskItem
+            {
+                Id = 1,
+                Title = "Изучить ASP.NET Core",
+                Description = "Контроллеры, маршруты, middleware",
+                Priority = "High",
+                IsCompleted = true,
+                CreatedAt = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+            },
+            new TaskItem
+            {
+                Id = 2,
+                Title = "Подключить SQLite через EF Core",
+                Description = "Миграции, DbContext, LINQ-запросы",
+                Priority = "High",
+                IsCompleted = false,
+                CreatedAt = new DateTime(2026, 1, 2, 0, 0, 0, DateTimeKind.Utc)
+            },
+            new TaskItem
+            {
+                Id = 3,
+                Title = "Написть README",
+                Description = "Описать ст руктуру проекта",
+                Priority = "Normal",
+                IsCompleted = false,
+                CreatedAt = new DateTime(2026, 1, 3, 0, 0, 0, DateTimeKind.Utc)
+            }
+        );
+    }
+}
