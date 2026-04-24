af2cda86f0653697c64d3fdc4e6b95472facb23e
diishqa <diana.34@list.ru>
2026-04-24 20:23:30 +0300

    Step 4: InitialCreate migration applied, taskdb.db created

---
+++ TaskDb/Migrations/20260424172139_НазваниеМиграции.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskD.Migrations
+{
+    /// <inheritdoc />
+    public partial class НазваниеМиграции : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
